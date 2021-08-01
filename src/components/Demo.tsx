6239df886453c205c886edf4e074490ecba93b18
huyennv <phillipng.working@gmail.com>
2021-08-01 17:22:28 +0700

    clear redundant info

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,12 +1,4 @@
-import React from "react";
 import useFetch from "../custom-hooks/useFetch";
-
-// interface Pok {
-//   userId: number
-//   id: number
-//   title: string
-//   body: string
-// }
 
 function Demo() {
   const { status, data, error } = useFetch<any>("/ditto");
